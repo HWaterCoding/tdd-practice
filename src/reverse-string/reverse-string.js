d51c2ba1b5fa9a35fb026b14debc537f6514a9d7
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-05-30 16:59:59 -0400

    Create 3 tests for reverseString() Make the reverseString() function pass all 3 tests

---
+++ src/reverse-string/reverse-string.js
@@ -0,0 +1,6 @@
+export default function reverseString(string){
+    if(typeof string !== "string"){
+        throw new Error("Must be a string to reverse");
+    }
+    return string.split("").reverse().join("").toLowerCase();
+}
