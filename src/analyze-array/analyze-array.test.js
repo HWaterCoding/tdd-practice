d4fa927d82b795599146f8b6e2e71b5d5cd3ffc4
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-06-03 00:41:43 -0400

    Fix property tests and add empty array test

--- src/analyze-array/analyze-array.test.js
+++ src/analyze-array/analyze-array.test.js
@@ -24,17 +24,24 @@ test("works with different numbers", ()=>{
 
 //Individual tests to return all 4 object properties
 test("finds the average", ()=>{
-    expect(analyzeArray([1, 2, 3, 4, 5])).toBe(3);
+    expect(analyzeArray([2, 4, 6]).average).toBe(4);
 });
 
 test("finds the minimum", ()=>{
-    expect(analyzeArray([1, 2, 3, 4, 5])).toBe(1);
+    expect(analyzeArray([8, 4, 10]).min).toBe(4);
 });
 
 test("finds the maximum", ()=>{
-    expect(analyzeArray([1, 2, 3, 4, 5])).toBe(5);
+    expect(analyzeArray([8, 4, 10]).max).toBe(10);
 });
 
 test("finds the length", ()=>{
-    expect(analyzeArray([1, 2, 3, 4, 5])).toBe(5);
+    expect(analyzeArray([1, 2, 3]).length).toBe(3);
+});
+
+
+
+test("rejects empty arrays", () => {
+    expect(() => analyzeArray([]))
+        .toThrow();
 });
